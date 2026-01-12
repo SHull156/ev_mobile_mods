6ec32e4ee9baefd331d9d86e95513ef739786cda
SHull156 <sarahthull@gmail.com>
2026-01-12 17:04:05 +0000

    Added hamburger menu and updated site styling

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 document.getElementById("year").textContent = new Date().getFullYear();
+
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.nav');
+
+menuToggle.addEventListener('click', (e) => {
+    e.preventDefault();
+    nav.classList.toggle('visible');
+    
+});
